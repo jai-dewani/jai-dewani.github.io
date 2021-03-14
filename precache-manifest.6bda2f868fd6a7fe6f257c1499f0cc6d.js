0307271afc11b93d393401318ee41bdfbe259c30
jai-dewani <jai-dewani@users.noreply.github.com>
2021-03-14 14:16:53 +0000

    deploy: bb4a7ae2e7823ed8556418ab492e0c5aac1c5c89

---
+++ precache-manifest.6bda2f868fd6a7fe6f257c1499f0cc6d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81fe87a6ad4ad2bfd95c59aa8adaf746",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bd8b5154d3abe6e1423",
+    "url": "/static/css/main.3f3fcadd.chunk.css"
+  },
+  {
+    "revision": "841eb2906edf447a2965",
+    "url": "/static/js/2.2fd2ace9.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.2fd2ace9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3bd8b5154d3abe6e1423",
+    "url": "/static/js/main.95bf5aac.chunk.js"
+  },
+  {
+    "revision": "0adeb7b1d8f47cd9bf9f",
+    "url": "/static/js/runtime-main.71617d02.js"
+  },
+  {
+    "revision": "8036009e4c48421bfd59016b76ba48d8",
+    "url": "/static/media/Jai-small.8036009e.jpg"
+  },
+  {
+    "revision": "487c46655ea91c50b2a3c3c39c630060",
+    "url": "/static/media/arrow-right.487c4665.svg"
+  },
+  {
+    "revision": "7887c80e8534a0d3e1a52f1b26f5d21c",
+    "url": "/static/media/codechef.7887c80e.svg"
+  },
+  {
+    "revision": "70ad20d6d78bbbfd9c9254f8870b7450",
+    "url": "/static/media/codeforces.70ad20d6.svg"
+  },
+  {
+    "revision": "39c094f9f51fb898078a09cdea6e05b4",
+    "url": "/static/media/envelope.39c094f9.svg"
+  },
+  {
+    "revision": "f5a069d24c7855d24188c14f570b6a57",
+    "url": "/static/media/github.f5a069d2.svg"
+  },
+  {
+    "revision": "6f7f616f44e4c4244580338661f876d9",
+    "url": "/static/media/instagram.6f7f616f.svg"
+  },
+  {
+    "revision": "22bd17393aa39a4cfec33c206db5f0df",
+    "url": "/static/media/linkedin-in.22bd1739.svg"
+  },
+  {
+    "revision": "0729c42ac6c88ea12311c6989df785d6",
+    "url": "/static/media/twitter.0729c42a.svg"
+  }
+]);
